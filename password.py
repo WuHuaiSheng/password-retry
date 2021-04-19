2b0bbe30acd390b46e91548c3abbc4d76777b65d
WuHuaiSheng <zxc20044123@gmail.com>
2021-04-19 23:45:03 +0800

    Add comment

--- password.py
+++ password.py
@@ -1,11 +1,11 @@
 password = 'a123456'
-x = 3
+x = 3 #剩餘機會
 while x > 0:
 	x = x - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
-		break
+		break #逃出迴圈
 	else:
 		print('密碼錯誤!')
 		if x > 0:
